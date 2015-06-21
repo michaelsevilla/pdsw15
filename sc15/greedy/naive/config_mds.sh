fa623c762f50edcc5ec587bd784954d604583431
Michael Sevilla <mikesevilla3@gmail.com>
2015-06-20 22:50:45 -0700

    experiments: add mds configs for sc experiments

---
+++ sc15/greedy/naive/config_mds.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Greedy spill balancer
+
+WHOAMI=`cat /etc/hostname`
+$CEPH_SET="sudo ceph --admin-daemon /var/run/ceph/ceph-mds.$WHOAMI.asok config set"
+echo -n "2" > /tmp/balancer_state
+
+# Greedy spill balancer
+
+{
+    WHOAMI=`cat /etc/hostname`
+    $CEPH_SET="sudo ceph --admin-daemon /var/run/ceph/ceph-mds.$WHOAMI.asok config set"
+    echo -n "2" > /tmp/balancer_state
+    
+    # Debugging
+    $CEPH_SET debug_mds 0
+    $CEPH_SET debug_mds_migrator 0
+    $CEPH_SET debug_mds_balancer 10
+    $CEPH_SET mds_bal_print_dfs 10
+    $CEPH_SET mds_bal_print_dfs_metaload 0.5
+    $CEPH_SET mds_bal_print_dfs_depth 4
+    
+    # Turn on/off features
+    $CEPH_SET mds_bal_dir $DIR
+    $CEPH_SET mds_log true
+    $CEPH_SET mds_bal_frag 1
+    $CEPH_SET mds_cache_size 0
+    $CEPH_SET client_cache_size 0
+    
+    # Configuration parameters (i.e. tunables)
+    $CEPH_SET mds_bal_need_min 0.98
+    $CEPH_SET mds_bal_split_size 50000
+    $CEPH_SET ms_async_op_threads 0
+    
+    # balancer
+    $CEPH_SET mds_bal_metaload "IWR"
+    $CEPH_SET mds_bal_mdsload "MDSs[i][\"all\"]"
+    $CEPH_SET mds_bal_when "if_MDSs[whoami][\"load\"]>.01_and_MDSs[whoami+1][\"load\"]<0.01_then"
+    $CEPH_SET mds_bal_where "targets[whoami+1]=MDSs[whoami][\"load\"]/2"
+    $CEPH_SET mds_bal_howmuch "{\"half\"}"
+} 2>&1 | grep "success\|failure"
+
